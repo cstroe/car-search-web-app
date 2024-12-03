4387c7f4d3c5c707565ba7bc20d033f2fee42be8
Cosmin Stroe <oncall@cosmin.cloud>
2024-12-03 09:15:24 -0600

    use scalafmt manually

--- src/main/scala/com/example/ScalatraMain.scala
+++ src/main/scala/com/example/ScalatraMain.scala
@@ -1,14 +1,13 @@
 package com.example
 
-import org.eclipse.jetty.ee10.servlet.DefaultServlet
 import org.eclipse.jetty.ee10.webapp.WebAppContext
 import org.eclipse.jetty.server.Server
 import org.scalatra.servlet.ScalatraListener
 
 object ScalatraMain {
   def main(args: Array[String]): Unit = {
-    val port = 8082
-    val server = new Server(port)
+    val port    = 8082
+    val server  = new Server(port)
     val context = new WebAppContext()
     context.setContextPath("/")
     context.setBaseResourceAsString("src/main/webapp")
